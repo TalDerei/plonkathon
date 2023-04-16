57201db9bc913fcebd7e50b395163352518a0cc4
Tal Derei <Tad222@lehigh.edu>
2023-04-16 14:29:35 -0400

    added step-1 -- creating kzg commitment and verification key object

--- test.py
+++ test.py
@@ -15,24 +15,23 @@ def setup_test():
     print("===setup_test===")
 
     setup = Setup.from_file("test/powersOfTau28_hez_final_11.ptau")
-    dummy_values = Polynomial(
-        list(map(Scalar, [1, 2, 3, 4, 5, 6, 7, 8])), Basis.LAGRANGE
-    )
+    dummy_values = Polynomial(list(map(Scalar, [1, 2, 3, 4, 5, 6, 7, 8])), Basis.LAGRANGE)
     program = Program(["c <== a * b"], 8)
     commitment = setup.commit(dummy_values)
+    
     assert commitment == G1Point(
         (
             16120260411117808045030798560855586501988622612038310041007562782458075125622,
             3125847109934958347271782137825877642397632921923926105820408033549219695465,
         )
     )
+    
     vk = setup.verification_key(program.common_preprocessed_input())
     assert (
         vk.w
         == 19540430494807482326159819597004422086093766032135589407132600596362845576832
     )
     print("Successfully created dummy commitment and verification key")
-
 
 def basic_test():
     print("===basic_test===")
@@ -107,7 +106,7 @@ def prover_test_dummy_verifier(setup):
     program = Program(["e public", "c <== a * b", "e <== c * d"], 8)
     assignments = {"a": 3, "b": 4, "c": 12, "d": 5, "e": 60}
     prover = Prover(setup, program)
-    proof = prover.prove(assignments)
+    proof = prover.prove(assignments) # type: ignore
 
     print("Beginning test verification")
     program = Program(["e public", "c <== a * b", "e <== c * d"], 8)
@@ -140,7 +139,7 @@ def prover_test(setup):
     program = Program(["e public", "c <== a * b", "e <== c * d"], 8)
     assignments = {"a": 3, "b": 4, "c": 12, "d": 5, "e": 60}
     prover = Prover(setup, program)
-    proof = prover.prove(assignments)
+    proof = prover.prove(assignments) # type: ignore
     print("Prover test success")
     return proof
 
@@ -262,6 +261,7 @@ def poseidon_test(setup):
 if __name__ == "__main__":
     # Step 1: Pass setup test
     setup_test()
+    exit(0)
 
     setup = basic_test()
 
